31b1da6d0dfb69aa35dc4ab7351f5a3c76f6d0ca
Rod Johnson <johnsonroda@gmail.com>
2025-12-25 09:43:57 +1000

    Upgrade to 0.3.1

--- src/main/java/com/embabel/examples/ragbot/RagbotApplication.java
+++ src/main/java/com/embabel/examples/ragbot/RagbotApplication.java
@@ -15,14 +15,11 @@
  */
 package com.embabel.examples.ragbot;
 
-import com.embabel.agent.config.annotation.EnableAgents;
-import com.embabel.agent.config.annotation.LoggingThemes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-@EnableAgents(loggingTheme = LoggingThemes.COLOSSUS)
 class RagbotApplication {
     public static void main(String[] args) {
         SpringApplication.run(RagbotApplication.class, args);
