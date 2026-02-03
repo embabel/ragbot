eab44be8a6237e8db212d3e678f63de96e925b8d
Rod Johnson <johnsonroda@gmail.com>
2026-02-04 09:18:21 +1100

    Multi rag test

--- src/main/java/com/embabel/examples/ragbot/ChatActions.java
+++ src/main/java/com/embabel/examples/ragbot/ChatActions.java
@@ -3,12 +3,49 @@ package com.embabel.examples.ragbot;
 import com.embabel.agent.api.annotation.Action;
 import com.embabel.agent.api.annotation.EmbabelComponent;
 import com.embabel.agent.api.common.ActionContext;
+import com.embabel.agent.rag.model.Chunk;
+import com.embabel.agent.rag.model.Retrievable;
 import com.embabel.agent.rag.service.SearchOperations;
+import com.embabel.agent.rag.service.TextSearch;
 import com.embabel.agent.rag.tools.ToolishRag;
 import com.embabel.chat.Conversation;
 import com.embabel.chat.UserMessage;
+import com.embabel.common.core.types.SimilarityResult;
+import com.embabel.common.core.types.SimpleSimilaritySearchResult;
+import com.embabel.common.core.types.TextSimilaritySearchRequest;
+import org.jspecify.annotations.NonNull;
 
+import java.util.List;
 import java.util.Map;
+
+
+class CarSearchOperations implements TextSearch {
+
+    @Override
+    public @NonNull String getLuceneSyntaxNotes() {
+        return "only keywords";
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public @NonNull <T extends Retrievable> List<SimilarityResult<T>> textSearch(@NonNull TextSimilaritySearchRequest request, @NonNull Class<T> clazz) {
+        return List.of(
+                (SimilarityResult<T>) new SimpleSimilaritySearchResult<>(
+                        Chunk.create("The Holden Commodore was initially based on an Opel design", "parent1"),
+                        0.95
+                ),
+                (SimilarityResult<T>) new SimpleSimilaritySearchResult<>(
+                        Chunk.create("The HQ Holden was a huge seller in Australia in the early 70s", "parent2"),
+                        0.90
+                )
+        );
+    }
+
+    @Override
+    public boolean supportsType(@NonNull String type) {
+        return type.equals("Chunk");
+    }
+}
 
 /**
  * The platform can use any action to respond to user messages.
@@ -16,16 +53,13 @@ import java.util.Map;
 @EmbabelComponent
 public class ChatActions {
 
-    private final ToolishRag toolishRag;
+    private final SearchOperations searchOperations;
     private final RagbotProperties properties;
 
     public ChatActions(
             SearchOperations searchOperations,
             RagbotProperties properties) {
-        this.toolishRag = new ToolishRag(
-                "sources",
-                "Classic music criticism",
-                searchOperations);
+        this.searchOperations = searchOperations;
         this.properties = properties;
     }
 
@@ -38,11 +72,19 @@ public class ChatActions {
             ActionContext context) {
         // We could use a simple prompt here but choose to use a template
         // as chatbots tend to require longer prompts
+        var musicRag = new ToolishRag(
+                "sources",
+                "Classic music criticism",
+                searchOperations);
+        var carRag = new ToolishRag(
+                "car_sources",
+                "Car data",
+                new CarSearchOperations());
         var assistantMessage = context.
                 ai()
                 .withLlm(properties.chatLlm())
-                .withReference(toolishRag)
-                .withTemplate("ragbot")
+                .withReferences(musicRag, carRag)
+                .rendering("ragbot")
                 .respondWithSystemPrompt(conversation, Map.of(
                         "properties", properties,
                         "voice", properties.voice(),
